3d59f30f26f233dc05806e904f30c312f65b2d7e
Tom <tombinnie@protonmail.com>
2022-07-31 23:25:04 +0100

    create core components

---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Hero = (props) => {
+    return (
+        <h1>Hero</h1>
+    )
+}
+
+export default Hero
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = (props) => {
+    return (
+        <div className="navbar">
+            <div>Top</div>
+            <div>Skills</div>
+            <div>Projects</div>
+            <div>Contact</div>
+        </div>
+    )
+}
+
+export default Navbar
