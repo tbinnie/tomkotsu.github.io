d3de8e345954deb48f1a179110dcc2a5d15e9756
Tom <tombinnie@protonmail.com>
2022-10-14 11:50:15 +0100

    adjust fonts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,28 +9,46 @@ import githubIcon from '../imgs/contact-icons/github-icon.svg'
 const Contact = () => {
   return (
     <section className="contact" id="contact">
-      <h2>
+      <h2 className='touch-title'>
         <span className="number">03. Get In Touch?</span>
       </h2>
-      <h1>Contact me!</h1>
-      <p>
+      <h1 className='contact-title'>Contact me!</h1>
+      <p className='contact-text'>
         I am currently actively looking for full-time Frontend Development
         positions. I would love to hear about any opportunities, and I can be
         contacted using the links below.
       </p>
       <div className="contactBtns">
-        <a href="mailto:tombinnie@protonmail.com" className='contact-link'>
-          <button className='btn'><img src={emailIcon} /> Email me!</button>
-
+        <a href="mailto:tombinnie@protonmail.com" className="contact-link">
+          <button className="btn">
+            <img src={emailIcon} /> Email me!
+          </button>
         </a>
-        <a href={pdf} target="_blank" className='contact-link'>
-          <button className='btn'><img src={cvIcon} />My CV</button>
+        <a href={pdf} target="_blank" className="contact-link">
+          <button className="btn">
+            <img src={cvIcon} />
+            My CV
+          </button>
         </a>
-        <a href="https://github.com/tomkotsu" target="_blank" className='contact-link'>
-          <button className="btn"><img src={githubIcon} />Github</button>
+        <a
+          href="https://github.com/tomkotsu"
+          target="_blank"
+          className="contact-link"
+        >
+          <button className="btn">
+            <img src={githubIcon} />
+            Github
+          </button>
         </a>
-        <a href="https://www.linkedin.com/in/tom-binnie/" target="_blank" className='contact-link'>
-          <button className='btn'><img src={linkedinIcon} />LinkedIn</button>
+        <a
+          href="https://www.linkedin.com/in/tom-binnie/"
+          target="_blank"
+          className="contact-link"
+        >
+          <button className="btn">
+            <img src={linkedinIcon} />
+            LinkedIn
+          </button>
         </a>
       </div>
     </section>
